f5717718001b1c37e0de2a676e5823b0c970cfa2
Richard Aubrey White <hello@rwhite.no>
2025-11-21 10:02:10 +0300

    Update base image for Kubernetes container

--- cs9example_weather_dag.py
+++ cs9example_weather_dag.py
@@ -49,7 +49,7 @@ try:
                     containers=[
                         k8s.V1Container(
                             name="base",
-                            image="ghcr.io/fhidev/fhi.fida.cs/cs9base-k8s:latest",
+                            image="ghcr.io/niphr/cs/rbase:4.5.1",
                             image_pull_policy="Always",
                             resources=CS9_CONTAINER_RESOURCES,
                             env=[
